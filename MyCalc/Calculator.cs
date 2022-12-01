9b37ad9d59873f67e0fd9fc3d4d48cbdf4197e34
ShanKonduru <ShanKonduru@gmail.com>
2022-12-01 12:45:00 +0530

    PractiTest VS Code Integration

---
+++ MyCalc/Calculator.cs
@@ -0,0 +1,70 @@
+﻿namespace MyCalc;
+public static class Calculator
+{
+    public static int Add(int operand1, int operand2)
+    {
+        return operand1 + operand2;
+    }
+
+    public static int Sub(int operand1, int operand2)
+    {
+        return operand1 - operand2;
+    }
+
+    public static int Mul(int operand1, int operand2)
+    {
+        return operand1 * operand2;
+    }
+
+    public static float Div(float operand1, float operand2)
+    {
+        float result = operand1 / operand2;
+        return result;
+    }
+
+    public static bool FloatNearlyEqual(float a, float b, float epsilon)
+    {
+        const float MinNormal = float.MinValue;
+        float absA = Math.Abs(a);
+        float absB = Math.Abs(b);
+        float diff = Math.Abs(a - b);
+
+        if (a.Equals(b))
+        { // shortcut, handles infinities
+            return true;
+        }
+        else if (a == 0 || b == 0 || absA + absB < MinNormal)
+        {
+            // a or b is zero or both are extremely close to it
+            // relative error is less meaningful here
+            return diff < (epsilon * MinNormal);
+        }
+        else
+        { // use relative error
+            return diff / (absA + absB) < epsilon;
+        }
+    }
+
+    public static bool DoubleNearlyEqual(double a, double b, double epsilon = 0.00000000001)
+    {
+        const double MinNormal = 2.2250738585072014E-308d;
+        double absA = Math.Abs(a);
+        double absB = Math.Abs(b);
+        double diff = Math.Abs(a - b);
+
+        if (a.Equals(b))
+        { // shortcut, handles infinities
+            return true;
+        }
+        else if (a == 0 || b == 0 || absA + absB < MinNormal)
+        {
+            // a or b is zero or both are extremely close to it
+            // relative error is less meaningful here
+            return diff < (epsilon * MinNormal);
+        }
+        else
+        { // use relative error
+            return diff / (absA + absB) < epsilon;
+        }
+    }
+}
---
+++ TestMyCalc/UnitTestCalculator.cs
@@ -0,0 +1,169 @@
+using System;
+using MyCalc;
+
+namespace TestMyCalc;
+
+[TestClass]
+public class UnitTestCalculator
+{
+    public TestContext? TestContext { get; set; }
+
+    private static TestContext? _testContext;
+
+    [ClassInitialize]
+    public static void SetupTests(TestContext testContext)
+    {
+        _testContext = testContext;
+    }
+
+    [TestMethod]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Add")]
+    [TestCategory("Regression")]
+    public void TestCalculatorAddMethod()
+    {
+        int result = MyCalc.Calculator.Add(10, 20);
+        Assert.AreEqual(30, result, "Add method failed.");
+    }
+
+        [TestMethod]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Div")]
+    [Ignore]
+    [TestCategory("Regression")]
+    public void TestCalculatorDivMethod()
+    {
+        float expected = 3.3333F;
+        float result = MyCalc.Calculator.Div(10, 3);
+        Assert.AreEqual(3.3333, result, "Div method failed.");
+        Assert.IsTrue(Calculator.FloatNearlyEqual(expected, result, 0.00001F), "Div method failed.");
+    }
+
+
+    [DataRow(0, 0, 0)]
+    [DataRow(1, 1, 2)]
+    [DataRow(2, 1, 3)]
+    [DataRow(80, 20, 100)]
+    [DataTestMethod]
+    [TestCategory("Add")]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Regression")]
+    public void DataTestCalculatorAddMethod(int operand1, int operand2, int expectedresult)
+    {
+        int result = MyCalc.Calculator.Add(operand1, operand2);
+        Assert.AreEqual(expectedresult, result, "Add method failed.");
+    }
+
+    [DataTestMethod]
+    [TestCategory("Sub")]
+    [DynamicData(nameof(SubGetData), DynamicDataSourceType.Method)]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Regression")]
+    public void TestSubDynamicDataMethod(int a, int b, int expected)
+    {
+        int result = MyCalc.Calculator.Sub(a, b);
+        Assert.AreEqual(expected, result, "Sub method failed.");
+    }
+    public static IEnumerable<object[]> SubGetData()
+    {
+        yield return new object[] { 10, 5, 5 };
+        yield return new object[] { 30, 15, 15 };
+        yield return new object[] { 30, 20, 10 };
+    }
+
+    [DataTestMethod]
+    [TestCategory("Mul")]
+    [DynamicData(nameof(MulGetData), DynamicDataSourceType.Method)]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Regression")]
+    public void TestMulDynamicDataMethod(int a, int b, int expected)
+    {
+        int result = MyCalc.Calculator.Mul(a, b);
+        Assert.AreEqual(expected, result, "Mul method failed.");
+    }
+    public static IEnumerable<object[]> MulGetData()
+    {
+        yield return new object[] { 10, 5, 50 };
+        yield return new object[] { 30, 15, 450 };
+        yield return new object[] { 30, 20, 600 };
+    }
+
+    [DataTestMethod]
+    [TestCategory("Add")]
+    [TestCategory("PositiveTest")]
+    [DynamicData(nameof(AddGetData), DynamicDataSourceType.Method)]
+    [TestCategory("Regression")]
+    public void TestAddDynamicDataMethod(int a, int b, int expected)
+    {
+        int result = MyCalc.Calculator.Add(a, b);
+        Assert.AreEqual(expected, result, "Add method failed.");
+    }
+    public static IEnumerable<object[]> AddGetData()
+    {
+        yield return new object[] { 1, 1, 2 };
+        yield return new object[] { 12, 30, 42 };
+        yield return new object[] { 14, 1, 15 };
+    }
+
+    [TestMethod]
+    [Ignore]
+    [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "DataDrivenTest.csv", "DataDrivenTest#csv", DataAccessMethod.Sequential)]
+    [TestCategory("Regression")]
+    [TestCategory("PositiveTest")]
+    public void DataDrivenTest()
+    {
+        // int valueA = Convert.ToInt32(_testContext.DataRow["Op1"]);
+        // int valueB = Convert.ToInt32(_testContext.DataRow["Op2"]);
+        // int expected = Convert.ToInt32(_testContext.DataRow["expectedResult"]);
+    }
+
+    [TestMethod]
+    [TestCategory("NegativeTest")]
+    [TestCategory("Add")]
+    [TestCategory("Regression")]
+    public void TestCalculatorAddMethod_NegativeNumbers()
+    {
+        int result = MyCalc.Calculator.Add(-10, -20);
+        Assert.AreEqual(-30, result, "Add method failed.");
+    }
+
+    [TestMethod]
+    [TestCategory("NegativeTest")]
+    [TestCategory("Add")]
+    [TestCategory("Regression")]
+    public void TestCalculatorAddMethod_PositiveAndNegativeNumbers()
+    {
+        int result = MyCalc.Calculator.Add(-10, 20);
+        Assert.AreEqual(10, result, "Add method failed.");
+    }
+
+    [TestMethod]
+    [TestCategory("PositiveTest")]
+    [TestCategory("Sub")]
+    [TestCategory("Regression")]
+    public void TestCalculatorSubMethod()
+    {
+        int result = MyCalc.Calculator.Sub(10, 20);
+        Assert.AreEqual(-10, result, "Sub method failed.");
+    }
+
+    [TestMethod]
+    [TestCategory("NegativeTest")]
+    [TestCategory("Sub")]
+    [TestCategory("Regression")]
+    public void TestCalculatorSubMethod_NegativeNumbers()
+    {
+        int result = MyCalc.Calculator.Sub(-10, -20);
+        Assert.AreEqual(10, result, "Sub method failed.");
+    }
+
+    [TestMethod]
+    [TestCategory("NegativeTest")]
+    [TestCategory("Sub")]
+    [TestCategory("Regression")]
+    public void TestCalculatorSubMethod_PositiveAndNegativeNumbers()
+    {
+        int result = MyCalc.Calculator.Sub(-10, 20);
+        Assert.AreEqual(-30, result, "Sub method failed.");
+    }
+}
