68f8059c9862fa41bbad7b0c6b1ae1f409521e85
ShanKonduru <ShanKonduru@gmail.com>
2022-12-02 21:37:49 +0530

    added Package

--- MyCalc/obj/Release/net7.0/MyCalc.AssemblyInfo.cs
+++ MyCalc/obj/Release/net7.0/MyCalc.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyCalc")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ShanOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
